2afa17c7807f86abf091cca9f70d5a7ea604ec02
Maryam Khanmadova <116656578+MaryamKhanmadova@users.noreply.github.com>
2025-11-27 01:23:33 +0400

    Update CORS origin for email automation app

--- api/endpoints.js
+++ api/endpoints.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://email-automation-lime.vercel.app',
+    origin: 'https://email-automation-hr-maryams-projects-d97582b9.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }),
