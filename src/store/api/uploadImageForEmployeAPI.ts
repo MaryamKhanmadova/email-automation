f3d90753625c8745419be7173d1954dab909a096
Maryam Khanmadova <116656578+MaryamKhanmadova@users.noreply.github.com>
2025-11-27 12:25:59 +0400

    Change API base URL for image upload

--- src/store/api/uploadImageForEmployeAPI.ts
+++ src/store/api/uploadImageForEmployeAPI.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const uploadImageForEmployeApi = createApi({
   reducerPath: 'uploadImageForEmployeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://email-automation-hr.vercel.app/api' }),
   endpoints: (builder) => ({
     uploadImage: builder.mutation<{ filename: string }, FormData>({
       query: (formData) => ({
