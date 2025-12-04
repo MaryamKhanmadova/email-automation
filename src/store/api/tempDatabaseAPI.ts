3558745ed963d102d03eccc600076e42fe5d6ef9
Maryam Khanmadova <116656578+MaryamKhanmadova@users.noreply.github.com>
2025-12-04 14:07:20 +0400

    Update base URL for tempDatabaseApi

--- src/store/api/tempDatabaseAPI.ts
+++ src/store/api/tempDatabaseAPI.ts
@@ -7,7 +7,7 @@ interface NameEntry {
 
 export const tempDatabaseApi = createApi({
   reducerPath: 'tempDatabaseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://email-automation-hr.vercel.app/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://email-automation-7hrg.onrender.com/api' }),
   endpoints: (builder) => ({
     fetchNames: builder.query<NameEntry[], void>({
       query: () => 'tempDatabase',
