bcae4c672b56706c18e12b49164ffc33f14e61e6
Maryam Khanmadova <116656578+MaryamKhanmadova@users.noreply.github.com>
2025-12-04 14:08:01 +0400

    Update base URL for uploadImageForEmployeApi

--- src/store/api/uploadImageForEmployeAPI.ts
+++ src/store/api/uploadImageForEmployeAPI.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const uploadImageForEmployeApi = createApi({
   reducerPath: 'uploadImageForEmployeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://email-automation-hr.vercel.app/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://email-automation-7hrg.onrender.com/api' }),
   endpoints: (builder) => ({
     uploadImage: builder.mutation<{ filename: string }, FormData>({
       query: (formData) => ({
